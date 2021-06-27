ab17c09f71570c4ff0b11b6d746d8626a9fbd100
akshay khanna <akshaykhanna2493@gmail.com>
2021-06-28 01:11:19 +0530

    Frontend validations

--- views/helper.js
+++ views/helper.js
@@ -1,7 +1,4 @@
 function validateRange(){
-    //alert("here")
-   //alert(document.getElementById("start_date").value); 
-   //alert(document.getElementById("end_date").value);
 if(document.getElementById("start_date").value >document.getElementById("end_date").value){
     document.getElementById("end_date").focus();
     return false;
@@ -9,11 +6,11 @@ if(document.getElementById("start_date").value >document.getElementById("end_dat
 return true;
 }
 function validateForm(){
-    //alert("Ht")
+
     if(!validateRange()){
         alert("End date cannot be lesser than Start Date")
         return false;
     }
-    alert("hr");
+
  
 }
